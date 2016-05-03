76c5314195d1cbbd306a82c30227ee9146e2ab58
Alex Berdyshev <berdishew@gmail.com>
2016-05-03 15:17:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import ReduxAsyncConnect, {loadOnServer} from './modules/ReduxAsyncConnect';
+import {reducer, clearKey, loadSuccess, asyncConnect} from './modules/asyncConnect';
+
+export {
+    loadOnServer,
+    ReduxAsyncConnect,
+    reducer,
+    clearKey,
+    loadSuccess,
+    asyncConnect
+};
